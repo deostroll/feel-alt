0a163422f856312070055913cf267bb65f91773e
deostroll <deostroll@gmail.com>
2019-06-07 13:49:14 +0530

    modified literal - number tests

--- test/grammars/literal-expressions/numbers/numbers-literals.test.spec.js
+++ test/grammars/literal-expressions/numbers/numbers-literals.test.spec.js
@@ -1,17 +1,45 @@
 const { expect } = require('chai');
 const chalk = require('chalk');
 
-const { make } = require('./../../../../index');
+const { parse } = require('./../../../../lib/parser');
+const { _internals } = require('./../../../../lib/parser/helper');
+const Helper = _internals.Helper;
+const { parseString } = require('xml2js');
+const path = require('path');
+const fs = require('fs');
+
 describe(chalk.blue('Parsing number-literals'), () => {
     describe(chalk.green('Numbers'), () => {
-        
+        let mockHelper;
+
+        before('mock file', done => {
+            let dmnFile = path.resolve(__dirname, '../../../data/mock.dmn');
+            fs.readFile(dmnFile, { 'encoding' : 'utf8'}, (err, contents) => {
+                if(err) {
+                    done(err)
+                }
+                else {
+                    parseString(contents, (err, data) => {
+                        if(err) {
+                            done(err)
+                        }
+                        else {
+                            // mockDmnDocument = data;
+                            mockHelper = new Helper(data);
+                            done();
+                        }
+                    })
+                }
+            })
+        })
         it('should conform the +ve number to the expected ast node', () => {
             var expected_ast = {
                 type: 'number',
                 value: 1
             };
-            var input = 1;
-            var actual_ast = make(input);
+            var input = '1';
+            var actual_ast = parse(input, mockHelper);
+            
             expect(actual_ast).to.deep.equal(expected_ast);
         })
 
@@ -20,8 +48,9 @@ describe(chalk.blue('Parsing number-literals'), () => {
                 type: 'number',
                 value: 1.99999
             };
-            var input = 1.99999;
-            var actual_ast = make(input);
+            var input = '1.99999';
+            // var actual_ast = parse(input);
+            var actual_ast = parse(input, mockHelper);
             expect(actual_ast).to.deep.equal(expected_ast);
         });
 
@@ -30,8 +59,9 @@ describe(chalk.blue('Parsing number-literals'), () => {
                 type: 'number',
                 value: -1
             };
-            var input = -1;
-            var actual_ast = make(input);
+            var input = '-1';
+            // var actual_ast = make(input);
+            var actual_ast = parse(input, mockHelper);
             expect(actual_ast).to.deep.equal(expected_ast);
         });
 
@@ -40,8 +70,9 @@ describe(chalk.blue('Parsing number-literals'), () => {
                 type: 'number',
                 value: -1.99999
             };
-            var input = -1.99999;
-            var actual_ast = make(input);
+            var input = '-1.99999';
+            // var actual_ast = make(input);
+            var actual_ast = parse(input, mockHelper);
             expect(actual_ast).to.deep.equal(expected_ast);
         });
     });
